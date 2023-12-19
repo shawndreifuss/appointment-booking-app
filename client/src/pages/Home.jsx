7300d1e18e8f72bd0e46c746fb431ac6db3398e1
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-12-19 13:42:41 -0800

    added icons section on home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,20 @@
+import heroImg01 from "../assets/images/hero-img01.png";
+import heroImg02 from "../assets/images/hero-img02.png";
+import heroImg03 from "../assets/images/hero-img03.png";
+import icon01 from "../assets/images/icon01.png";
+import icon02 from "../assets/images/icon02.png";
+import icon03 from "../assets/images/icon03.png";
+import { Link } from "react-router-dom";
+import { BsArrowRight } from "react-icons/bs";
+
 const Home = () => {
   return (
     <>
       {/* ============= Hero Section =========== */}
-
-      <section className="hero_section pt-[60px] 2xl:h-[800px]">
+      <section className="hero__section pl-64 pr-64 pt-[60px] 2xl:h-[800px]">
         <div className="container">
           <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between ">
             {/* ============= Hero Content =========== */}
-
             <div>
               <div className="lg:w-[570px]">
                 <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px] ">
@@ -17,12 +24,131 @@ const Home = () => {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  laboris nisi ut aliquip ex ea commodo consequat.
                 </p>
+                <button className="btn"> Request an Appointment</button>
+              </div>
+              {/* ============= Hero Counter =========== */}
+              <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
+                <div>
+                  <h2 className="text-[36px] leading=[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    30+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Years of experience </p>
+                </div>
+                <div>
+                  <h2 className="text-[36px] leading=[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    15+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Clinic Location </p>
+                </div>
+                <div>
+                  <h2 className="text-[36px] leading=[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    100%
+                  </h2>
+                  <span className="w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Patient Satisfaction </p>
+                </div>
+              </div>
+            </div>
+            {/* ============= Hero Content =========== */}
+
+            <div className="flex gap-[30px] justify-end ">
+              <div>
+                <img className="w-full" src={heroImg01} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <img className="w-full mb-[30px]" src={heroImg02} alt="" />
+                <img src={heroImg03} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* ============= Hero Section End =========== */}
+
+      <section>
+        <div className="container">
+          <div className="lg:w-[470px] mx-auto">
+            <h2 className="heading text-center">
+              Providing the best <br /> Medical services
+            </h2>
+            <p className="text__para text-center">
+              World-Class care for everyone. Our health system offers unmatched
+              expert health care.
+            </p>
+          </div>
+
+          {/* ============= Icon Section =========== */}
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px] ">
+          {/*  ========= Icon 1 ============= */}
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon01} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headerColor font-[700] text-center">
+                  Find A Doctor
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  World-Class care for everyone. <br /> Our health system offers
+                  unmatched expert health care.
+                </p>
+
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
+              </div>
+            </div>
+
+            {/*  ========= Icon 2 ============= */}
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon02} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headerColor font-[700] text-center">
+                  Find A Location
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  World-Class care for everyone. <br /> Our health system offers
+                  unmatched expert health care.
+                </p>
+
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
+              </div>
+            </div>
+            {/*  ========= Icon 3 ============= */}
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon03} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headerColor font-[700] text-center">
+                  Book Appointment
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  World-Class care for everyone. <br /> Our health system offers
+                  unmatched expert health care.
+                </p>
+
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
               </div>
             </div>
           </div>
