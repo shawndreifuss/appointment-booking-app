8a8ab418779f8f49dba58976399f1f3f661bef93
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-12-19 20:31:18 -0800

    added FAQ section to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,16 @@ import heroImg03 from "../assets/images/hero-img03.png";
 import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
+import avatarIcon from "../assets/images/avatar-icon.png";
+import featureImage from "../assets/images/feature-img.png";
+import videoIcon from "../assets/images/video-icon.png";
+import faqImage from "../assets/images/faq-img.png";
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About";
 import ServiceList from "../components/Services/ServiceList";
+import DoctorList from "../components/Doctors/DoctorList";
+import FaqList from "../components/Faqs/FaqList";
 
 const Home = () => {
   return (
@@ -85,8 +91,8 @@ const Home = () => {
           </div>
 
           {/* ============= Icon Section =========== */}
-<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px] ">
-          {/*  ========= Icon 1 ============= */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px] ">
+            {/*  ========= Icon 1 ============= */}
             <div className="py-[30px] px-5">
               <div className="flex items-center justify-center">
                 <img src={icon01} alt="" />
@@ -157,30 +163,116 @@ const Home = () => {
         </div>
       </section>
       {/* ============= About Section Start =========== */}
-    <About />
-  {/* ============= Services Section =========== */}
+      <About />
+      {/* ============= Services Section =========== */}
 
-  <section>
-    <div className="container">
-      <div className="xl:w-[470px] mx-auto ">
-        <h2 className="heading text-center">Our Medical Services</h2>
-        <p className="text__para text-center">World-Class care for everyone Our health System offers unmatched, expert health care.</p>
-      </div>
-      <ServiceList />
-    </div>
-  </section>
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto ">
+            <h2 className="heading text-center">Our Medical Services</h2>
+            <p className="text__para text-center">
+              World-Class care for everyone Our health System offers unmatched,
+              expert health care.
+            </p>
+          </div>
+          <ServiceList />
+        </div>
+      </section>
 
+      {/*  ============= Feature Section ================ */}
 
-  {/*  ============= Feature Section ================ */}
+      <section>
+        <div className="container pr-64 pl-64">
+          <div className="flex items-center justify-between flex-col lg:flex-row">
+            {/*  ========= Feature Content  */}
+            <div className="xl:w-[670px]">
+              <h2 className="heading">
+                Get Virtual Treatment <br />
+                anytime.
+              </h2>
+              <ul className="pl-4">
+                <li className="text__para">
+                  1. Schedule the appointment directly.
+                </li>
+                <li className="text__para">
+                  2. Search for your physician here, and contact their office.
+                </li>
+                <li className="text__para">
+                  3. View our physicians who are accepting new patients, use the
+                  online scheduling tool to select an appointment time.
+                </li>
+              </ul>
+              <Link to="/">
+                <button className="btn">Learn More</button>
+              </Link>
+            </div>
 
-  <section>
-    <div className="container">
-      <div className="flex items-center justify-between flex-col lg:flex-row">
+            {/*  ========= Feature Image ===========  */}
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImage} alt="" />
+              <div className="w-[150px] ml-24 lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 p-2 pb-3 lg:px-4 lg:pb-[26px] rounded-[10px]">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
+                      Tue, 24
+                    </p>
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[600]">
+                      10:10AM
+                    </p>
+                  </div>
+                  <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:px-[9px]">
+                    <img src={videoIcon} alt="" />
+                  </span>
+                </div>
 
-        {/*  ========= Feature Content  */}
-      </div>
-    </div>
-  </section>
+                <div className="rounded-full w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4">
+                  Consultation
+                </div>
+
+                <div className="flex items center gap[-6px0 lg:gap-[10px] mt-2 lg:mt-[18px]">
+                  <img src={avatarIcon} alt="" />
+                  <h4 className="text-[10px] leading-3 lg:text-916px] lg:leading-[22px] font-[700] text-headingColor">
+                    Shawn Dreifuss
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* ============= Our Great Doctors =========== */}
+      <section>
+        <div className="container pr-64 pl-64">
+          <div className="xl:w-[470px] mx-auto ">
+            <h2 className="heading text-center">Our Great Doctors</h2>
+            <p className="text__para text-center">
+              World-Class care for everyone Our health System offers unmatched,
+              expert health care.
+            </p>
+          </div>
+
+          <DoctorList />
+        </div>
+      </section>
+
+      {/*  ============== Faq Section =============== */}
+
+      <section>
+        <div className="container pr-64 pl-64">
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImage} alt="" />
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">Most questions by our beloved patients</h2>
+
+              <FaqList />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
